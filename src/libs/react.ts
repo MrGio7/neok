9d583b5c33a9e92ae558cc83b2e623ea2c28aac7
Giorgi Bakashvili <lashagiorgi1233@gmail.com>
2023-12-07 12:48:28 +0400

    Remove unused renderError function

--- src/libs/react.ts
+++ src/libs/react.ts
@@ -7,13 +7,3 @@ export function render(res: Response, el: JSX.Element) {
 
   return res.contentType("text/html").send(html);
 }
-
-export function renderError(res: Response, message: string) {
-  const html = renderToStaticMarkup(Error({ error: message }));
-
-  return res
-    .header("HX-Reswap", "afterbegin transition:true")
-    .header("HX-Retarget", "#errors")
-    .contentType("text/html")
-    .send(html);
-}
