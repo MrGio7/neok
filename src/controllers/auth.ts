ab6f6279903e70a48d9c0f8a1b44137ecfe13880
Giorgi Bakashvili <lashagiorgi1233@gmail.com>
2023-12-08 16:36:51 +0400

    Update SameSite attribute in Set-Cookie header

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -42,7 +42,7 @@ export const login: RequestHandler = async (req, res) => {
       "Set-Cookie",
       `refreshToken=${refreshToken}; HttpOnly; Path=/; Max-Age=${
         7 * 24 * 60 * 60
-      }; SameSite=Strict; Secure;`,
+      }; SameSite=Strict;`,
     )
     .setHeader("HX-Redirect", "/")
     .sendStatus(302);
@@ -90,7 +90,7 @@ export const register: RequestHandler = async (req, res) => {
       "Set-Cookie",
       `refreshToken=${refreshToken}; HttpOnly; Path=/; Max-Age=${
         7 * 24 * 60 * 60
-      }; SameSite=Strict; Secure;`,
+      }; SameSite=Strict;`,
     )
     .setHeader("HX-Redirect", "/")
     .sendStatus(302);
